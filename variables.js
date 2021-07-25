ded69865524f58d89d9bed69b7cfa7ff48e310d9
andrebezada <a.bezada.maza@gmail.com>
2021-07-25 18:39:55 -0500

    avanzando

--- variables.js
+++ variables.js
@@ -2,7 +2,19 @@ var nombre = 'andre';
 
 //declarar
 var edad;
+
+edad
 //inicializar
 edad = 30;
 
 edad
+
+var elementos = ['computadora', 'celular'];
+
+var persona = {
+    nombre: 'andre',
+    edad = 24
+}
+
+persona;
+
