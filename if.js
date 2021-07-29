aca3744ff98e65b943fd514b464bd9058f6fb781
andrebezada <a.bezada.maza@gmail.com>
2021-07-28 23:35:31 -0500

    avanzando9

---
+++ if.js
@@ -0,0 +1,30 @@
+var edad= 18
+
+if (edad === 18) {
+    console.log('puedes votar, será tu primera votación');
+} else if( edad > 18) {
+    console.log('puedes votar de nuevo')
+} else if( edad > 18) {
+    console.log('puedes votar de nuevo')
+} else if( edad > 18) {
+    console.log('puedes votar de nuevo')
+} else if( edad > 18) {
+    console.log('puedes votar de nuevo')
+} else {
+    console.log('aun no puedes votar')
+}
+
+
+
+
+
+
+condition ? true : false; // sintaxis de operador ternario
+
+
+var numero = 1;
+
+var resultado = numero === 1 ? 'sí soy un uno' : 'no, no soy uno';
+
+
+
